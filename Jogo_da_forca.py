3f777989de5aa42d871fce237322897a803793a3
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-07-09 16:06:17 -0300

    Update Jogo_da_forca.py

--- Jogo_da_forca.py
+++ Jogo_da_forca.py
@@ -4,15 +4,27 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
 
-    while(not acertou and not enforcou):
-        print("Jogando...")
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
-
 
 if(__name__ == "__main__"):
     jogar()
